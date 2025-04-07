d36ad097e603b22b0fc4d4dd9ab4130d14b69af1
Vlad567823 <165555735+Vlad567823@users.noreply.github.com>
2025-04-07 19:16:47 +0300

    Initial commit

---
+++ js/base.js
@@ -0,0 +1,38 @@
+let celsius = 25
+let fahrenheit = (celsius * 9/5) + 32;
+console.log("Температура в Фаренгейтах:", fahrenheit);
+let daysInMonth = 30; 
+let hoursInMonth = daysInMonth * 24; 
+let minutesInMonth = hoursInMonth * 60;
+console.log("Годин в місяці:", hoursInMonth);
+console.log("Хвилин в місяці:", minutesInMonth);
+let health = 100; 
+let energy = 100; 
+health -= 10;
+energy -= 20; 
+console.log("Рівень здоров'я:", health);
+console.log("Рівень енергії:", energy);
+let totalPrice = 110
+let discount = 0.10
+let discountedPrice = totalPrice * (1 - discount)
+console.log("Знижена ціна:", discountedPrice)
+const floatNumber = 12.78;
+const roundedDown = Math.floor(floatNumber);
+console.log("Округлене число:", roundedDown);
+const floatString = "45.67";
+const parsedFloat = parseFloat(floatString); 
+console.log("Число з плаваючою комою:", parsedFloat);
+const intString = "123";
+const parsedInt = parseInt(intString); 
+console.log("Ціле число:", parsedInt);
+let number = 25;
+let sqrtNumber = Math.sqrt(number);
+console.log("Корінь числа:", sqrtNumber);
+const integer = 42;
+const StringNumber = "256";
+let convertedInt = parseInt(StringNumber)
+console.log("Ціле число",convertedInt)
+const convertedString  = integer.toString(StringNumber);
+console.log("Цілочисельне число,  ")
+
+
